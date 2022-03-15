ed469aaacfd134deaec19b02870edf913abdb268
SuhrobDev <suhrobqayumov4@gmail.com>
2022-03-15 18:28:16 +0500

    Initial commit

---
+++ app/src/main/java/com/example/layoutsweather/weather1/WeatherActivity1.kt
@@ -0,0 +1,12 @@
+package com.example.layoutsweather.weather1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.layoutsweather.R
+
+class WeatherActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather1)
+    }
+}
---
+++ app/src/main/java/com/example/layoutsweather/weather2/WeatherAcitvity2.kt
@@ -0,0 +1,13 @@
+package com.example.layoutsweather.weather2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.layoutsweather.R
+
+class WeatherAcitvity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather2)
+    }
+}
+//https://github.com/SuhrobDev/weather_ayouts.git
