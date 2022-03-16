0d2beb666b029adad08b63f1a6e0076966126b5b
SuhrobDev <suhrobqayumov4@gmail.com>
2022-03-16 13:01:14 +0500

    kirish is done

---
+++ app/src/main/java/com/example/layoutsweather/kirish_oynasi/KirishActivity.kt
@@ -0,0 +1,12 @@
+package com.example.layoutsweather.kirish_oynasi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.layoutsweather.R
+
+class KirishActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kirish)
+    }
+}
