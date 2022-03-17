d789f883c0dbc0c01a037873ce7a910d7e061a1a
SuhrobDev <suhrobqayumov4@gmail.com>
2022-03-17 18:46:54 +0500

    added package

---
+++ app/src/main/java/com/example/layoutsweather/LinerRecycleViewLayout/Followers/FollowersActivity.kt
@@ -0,0 +1,12 @@
+package com.example.layoutsweather.LinerRecycleViewLayout.Followers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.layoutsweather.R
+
+class FollowersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_followers)
+    }
+}
